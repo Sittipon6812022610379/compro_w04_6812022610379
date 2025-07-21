3fb1e1f634aae38d5c10f4d89ddb8f46eef8a50a
Sittipon_6812022610379 <s6812022610379@email.kmutnb.ac.th>
2025-07-21 14:50:56 +0700

    Add files via upload

---
+++ coding_w04-04-01.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    float score = 90.5;
+    printf("Score: %.1f\n" , score);
+    return 0;
+}
+// Code ทำงานได้ปกติเพราะ 90.5 เป็น Double และใส่ F ข้างหลัง 90.5 เพื่อระบุว่าเป็น float
